a09ddca60afcd19ae4fc738b16dba701ee0d32c1
ZholmanIrrada <irradazholman@gmail.com>
2025-05-27 13:14:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// Firebase конфигурациясы
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCuCW1G6bqxb8EI_YZZ2dpqnyDC1FXaRrw",
-  authDomain: "bagalausaity.firebaseapp.com",
-  databaseURL: "https://bagalausaity-default-rtdb.firebaseio.com",
-  projectId: "bagalausaity",
-  storageBucket: "bagalausaity.firebasestorage.app",
-  messagingSenderId: "159427702781",
-  appId: "1:159427702781:web:d56f44c70ec7ccbaeaf394",
-  measurementId: "G-KV7WSEHL3W"
-};
+<!-- Firestore SDK қажет -->
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"></script>
 
-// Firebase-ті инициализациялау
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyCuCW1G6bqxb8EI_YZZ2dpqnyDC1FXaRrw",
+    authDomain: "bagalausaity.firebaseapp.com",
+    databaseURL: "https://bagalausaity-default-rtdb.firebaseio.com",
+    projectId: "bagalausaity",
+    storageBucket: "bagalausaity.appspot.com",
+    messagingSenderId: "159427702781",
+    appId: "1:159427702781:web:d56f44c70ec7ccbaeaf394",
+    measurementId: "G-KV7WSEHL3W"
+  };
 
-// Бағалау формасына оқиға тіркеу
-document.getElementById("feedbackForm").addEventListener("submit", function(e) {
+  // Firebase-ті инициализациялау
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+  // Форманы жіберу
+  document.getElementById("feedbackForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
     const formData = {};
@@ -42,4 +44,5 @@ document.getElementById("feedbackForm").addEventListener("submit", function(e) {
             console.error("Сақтау кезінде қате шықты: ", error);
             alert("Қате орын алды. Қайта көріңіз.");
         });
-});
+  });
+</script>
